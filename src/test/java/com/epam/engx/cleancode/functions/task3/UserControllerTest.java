e823a62abd9d228d16eb0dcedd2369b752115cbe
Vitalii_Tymashkov <vitaliy.tima@gmail.com>
2021-03-09 23:05:29 +0100

    Task 3 - Test minor refactoring

--- src/test/java/com/epam/engx/cleancode/functions/task3/UserControllerTest.java
+++ src/test/java/com/epam/engx/cleancode/functions/task3/UserControllerTest.java
@@ -4,12 +4,15 @@ import org.junit.Test;
 
 public class UserControllerTest {
 
+    private static final String ADMIN = "admin";
+    private static final String PASSWORD = "123";
+
     private final UserControllerMock userController = new UserControllerMock();
 
     @Test
     public void shouldNotAuthenticateUser() {
         userController.setUserAuthenticator(new FalseUserAuthenticator());
-        userController.authenticateUser("admin", "123");
+        userController.authenticateUser(ADMIN, PASSWORD);
         userController.assertGenerateFailLoginResponseCalled();
     }
 
@@ -17,7 +20,7 @@ public class UserControllerTest {
     public void shouldAuthenticateUser() {
         TrueUserAuthenticatorMock trueUserAuthenticatorMock = new TrueUserAuthenticatorMock();
         userController.setUserAuthenticator(trueUserAuthenticatorMock);
-        userController.authenticateUser("admin", "123");
+        userController.authenticateUser(ADMIN, PASSWORD);
         userController.assertGenerateSuccessLoginResponseCalled();
         trueUserAuthenticatorMock.assertSetCurrentUserToSession();
     }
