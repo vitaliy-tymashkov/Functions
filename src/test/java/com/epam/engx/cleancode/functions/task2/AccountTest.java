eef5b73074a5feef3eae2d8b7e552365c202f1cb
Vitalii_Tymashkov <vitaliy.tima@gmail.com>
2021-03-09 22:51:50 +0100

    Task 2 - Moderate refactoring

--- src/test/java/com/epam/engx/cleancode/functions/task2/AccountTest.java
+++ src/test/java/com/epam/engx/cleancode/functions/task2/AccountTest.java
@@ -18,22 +18,34 @@ import static org.junit.Assert.assertEquals;
 
 public class AccountTest {
 
+    private static final int REVIEWS_TEN = 10;
+    private static final int REVIEWS_THIRTY = 30;
+    private static final String LEVEL_ONE_NUMBER = "1";
+    private static final String LEVEL_TWO_NUMBER = "2";
+    private static final Level LEVEL_ONE = new Level(LEVEL_ONE_NUMBER);
+    private static final Level LEVEL_TWO = new Level(LEVEL_TWO_NUMBER);
+    private static final Level LEVEL_DEFAULT = Level.defaultLevel();
+    private static final ReviewStub REVIEW_STUB_ZERO = new ReviewStub(0);
+    private static final ReviewStub REVIEW_STUB_NINE = new ReviewStub(9);
+    private static final ReviewStub REVIEW_STUB_TEN = new ReviewStub(10);
+    private static final ReviewStub REVIEW_STUB_30 = new ReviewStub(30);
+    private static final ReviewStub REVIEW_STUB_5 = new ReviewStub(5);
 
     private TreeMap<Integer, Level> levelMap = new TreeMap<>(Collections.reverseOrder());
 
     @Before
     public void setUp() throws Exception {
-        levelMap.put(10, new Level("1"));
-        levelMap.put(30, new Level("2"));
+        levelMap.put(REVIEWS_TEN, LEVEL_ONE);
+        levelMap.put(REVIEWS_THIRTY, new Level(LEVEL_TWO_NUMBER));
     }
 
-    @Test (expected = NotActiveAccountException.class)
+    @Test(expected = NotActiveAccountException.class)
     public void shouldThrowExceptionWhenAccountIsNotRegistered() {
         Account accountStub = new NotRegisteredAccountStub();
         accountStub.getActivityLevel();
     }
 
-    @Test (expected = NotActiveAccountException.class)
+    @Test(expected = NotActiveAccountException.class)
     public void shouldThrowExceptionWhenAccountIsNotVisiting() {
         Account accountStub = new NotVisitingAccountStub();
         accountStub.getActivityLevel();
@@ -41,34 +53,34 @@ public class AccountTest {
 
     @Test
     public void shouldGetDefaultLevelWhenThereAreNoAnswers() {
-        Account accountStub = new AccountStub(Arrays.<Review>asList(new ReviewStub(0)));
-        assertEquals(Level.defaultLevel(), accountStub.getActivityLevel());
+        Account accountStub = new AccountStub(Arrays.<Review>asList(REVIEW_STUB_ZERO));
+        assertEquals(LEVEL_DEFAULT, accountStub.getActivityLevel());
     }
 
     @Test
     public void shouldGetDefaultLevelWhenThereAreNotEnoughAnswers() {
-        Account accountStub = new AccountStub(Arrays.<Review>asList(new ReviewStub(9)));
-        assertEquals(Level.defaultLevel(), accountStub.getActivityLevel());
+        Account accountStub = new AccountStub(Arrays.<Review>asList(REVIEW_STUB_NINE));
+        assertEquals(LEVEL_DEFAULT, accountStub.getActivityLevel());
     }
 
     @Test
     public void shouldGetFirstLevelWhenThereAreEnoughAnswers() {
-        Account accountStub = new AccountStub(Arrays.<Review>asList(new ReviewStub(10)));
+        Account accountStub = new AccountStub(Arrays.<Review>asList(REVIEW_STUB_TEN));
         accountStub.setLevelMap(levelMap);
-        assertEquals(new Level("1"), accountStub.getActivityLevel());
+        assertEquals(LEVEL_ONE, accountStub.getActivityLevel());
     }
 
     @Test
     public void shouldGetFirstLevelWhenThereAreEnoughSumOfAnswers() {
-        Account accountStub = new AccountStub(Arrays.<Review>asList(new ReviewStub(5), new ReviewStub(5)));
+        Account accountStub = new AccountStub(Arrays.<Review>asList(REVIEW_STUB_5, REVIEW_STUB_5));
         accountStub.setLevelMap(levelMap);
-        assertEquals(new Level("1"), accountStub.getActivityLevel());
+        assertEquals(LEVEL_ONE, accountStub.getActivityLevel());
     }
 
     @Test
     public void shouldGetSecondLevelWhenThereAreEnoughAnswers() {
-        Account accountStub = new AccountStub(Arrays.<Review>asList(new ReviewStub(30)));
+        Account accountStub = new AccountStub(Arrays.<Review>asList(REVIEW_STUB_30));
         accountStub.setLevelMap(levelMap);
-        assertEquals(new Level("2"), accountStub.getActivityLevel());
+        assertEquals(LEVEL_TWO, accountStub.getActivityLevel());
     }
 }
